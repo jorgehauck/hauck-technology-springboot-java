20d631b8240e20b9f0d8e0a42d406492af582723
José Jorge <jorge.hauck1997@outlook.com>
2022-01-31 19:34:21 -0300

    Project created

---
+++ src/main/java/com/haucktechnology/HauckTechnologyApplication.java
@@ -0,0 +1,13 @@
+package com.haucktechnology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HauckTechnologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HauckTechnologyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haucktechnology/HauckTechnologyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haucktechnology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HauckTechnologyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
