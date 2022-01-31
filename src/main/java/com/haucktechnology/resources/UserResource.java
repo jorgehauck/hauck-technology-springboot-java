a9fea5ce76357a7e7133568d61971c1cf24e367a
José Jorge <jorge.hauck1997@outlook.com>
2022-01-31 20:04:47 -0300

    User entity and resource

---
+++ src/main/java/com/haucktechnology/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.haucktechnology.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.haucktechnology.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User user = new User(1, "José Jorge", "jorge@gmail.com", "11970432348", "123456");
+
+		return ResponseEntity.ok(user);
+	}
+
+}
