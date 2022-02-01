877ed4649c9403837135e5ba7b8474327e242629
José Jorge <jorge.hauck1997@outlook.com>
2022-02-01 19:57:55 -0300

    Service layer, component registration

--- src/main/java/com/haucktechnology/resources/UserResource.java
+++ src/main/java/com/haucktechnology/resources/UserResource.java
@@ -1,21 +1,45 @@
 package com.haucktechnology.resources;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.haucktechnology.entities.User;
+import com.haucktechnology.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService userService;
+
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User user = new User(1, "José Jorge", "jorge@gmail.com", "11970432348", "123456");
+	public ResponseEntity<List<User>> findAll() {
+
+		List<User> user = userService.findAll();
 
 		return ResponseEntity.ok().body(user);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Optional<User>> findById(@PathVariable Integer id) {
+
+		Optional<User> user = userService.findById(id);
+
+		if (user != null) {
+			return ResponseEntity.ok().body(user);
+		}
+
+		else {
+			return ResponseEntity.notFound().build();
+		}
 	}
 
 }
---
+++ src/main/java/com/haucktechnology/services/UserService.java
@@ -0,0 +1,29 @@
+package com.haucktechnology.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.haucktechnology.entities.User;
+import com.haucktechnology.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	public List<User> findAll() {
+		List<User> user = userRepository.findAll();
+		return user;
+	}
+	
+	
+	public Optional<User> findById(Integer id) {
+		
+		Optional<User> user = userRepository.findById(id);
+		return user;
+	}
+}
